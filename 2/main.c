4bfa8468483d9ba7670030ccd1dec3cf73c2a40d
rrrusk <usagimikaduki@gmail.com>
2019-07-10 22:06:24 +0900

    first

---
+++ 2/main.c
@@ -0,0 +1,38 @@
+#include"bitmap.h"
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char *argv[])
+{
+//	if(argc != 3){
+//		fprintf(stderr, "Usage: program <inputfile> <outputfile>\n");
+//		exit(1);
+//	}
+
+	Image *colorimg;
+	Image *template;
+	Image *nccimg;
+
+	if((colorimg = Read_Bmp("img/gopochi-all.bmp")) == NULL){
+		exit(1);
+	}
+
+  if((template = Read_Bmp("img/gopochi-template.bmp")) == NULL){
+    exit(1);
+  }
+
+	nccimg = Create_Image((colorimg->width + template->width), (colorimg->height + template->height));
+
+  printf("%d\n", colorimg->width);
+  Template_Matching(colorimg, template, nccimg);
+	if(Write_Bmp("img/out.bmp", nccimg)){
+		exit(1);
+	}
+
+	Free_Image(colorimg);
+  Free_Image(nccimg);
+  Free_Image(template);
+
+	return 0;
+}
+
